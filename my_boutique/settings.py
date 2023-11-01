ed35fa625e38f1fb5620d174484be2ee4a8d7c07
zanettiprado <zanettiprado@gmail.com>
2023-11-01 20:22:27 +0000

    added homepage content

--- my_boutique/settings.py
+++ my_boutique/settings.py
@@ -62,9 +62,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'template'),
-            os.path.join(BASE_DIR, 'templates', 'allauth')
-            ],
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

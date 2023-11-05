455cd7e2e765ffe2a1c89d1ffa36f30015dbe377
zanettiprado <zanettiprado@gmail.com>
2023-11-05 14:42:46 +0000

    added sorting and product counts to products page

--- products/views.py
+++ products/views.py
@@ -13,7 +13,7 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
-    
+
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -21,19 +21,19 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-                
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
-                    sortkey = f'--{sortkey}'
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
             
-        products = products.order_by(sortkey)
-        
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-            
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -42,7 +42,7 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
@@ -50,7 +50,7 @@ def all_products(request):
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
-        }
+    }
 
     return render(request, 'products/products.html', context)
 
